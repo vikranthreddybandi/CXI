11e0a9d530cbf4233d76c78c11b15c1477f069a7
Vikranth Reddy Bandi <vikranth@wtastudios.com>
2019-08-29 17:47:56 +0530

    Sushma UI

--- src/app/main/QRconsumer/ConsumerSurveyComponents/StarRating.js
+++ src/app/main/QRconsumer/ConsumerSurveyComponents/StarRating.js
@@ -28,27 +28,27 @@ class StarRating extends React.Component {
         </div>
 
         <div class="star-rating">
-			<input id="star-5" type="radio" name="rating" value="star-5" />
-			<label for="star-5" title="5 stars">
-					<i class="active fa fa-star" aria-hidden="true"></i>
-			</label>
-			<input id="star-4" type="radio" name="rating" value="star-4" />
-			<label for="star-4" title="4 stars">
-					<i class="active fa fa-star" aria-hidden="true"></i>
-			</label>
-			<input id="star-3" type="radio" name="rating" value="star-3" />
-			<label for="star-3" title="3 stars">
-					<i class="active fa fa-star" aria-hidden="true"></i>
-			</label>
-			<input id="star-2" type="radio" name="rating" value="star-2" />
-			<label for="star-2" title="2 stars">
-					<i class="active fa fa-star" aria-hidden="true"></i>
-			</label>
-			<input id="star-1" type="radio" name="rating" value="star-1" />
-			<label for="star-1" title="1 star">
-					<i class="active fa fa-star" aria-hidden="true"></i>
-			</label>
-		</div>
+          <input id="star-5" type="radio" name="rating" value="star-5" />
+          <label for="star-5" title="5 stars">
+              <i class="active fa fa-star" aria-hidden="true"></i>
+          </label>
+          <input id="star-4" type="radio" name="rating" value="star-4" />
+          <label for="star-4" title="4 stars">
+              <i class="active fa fa-star" aria-hidden="true"></i>
+          </label>
+          <input id="star-3" type="radio" name="rating" value="star-3" />
+          <label for="star-3" title="3 stars">
+              <i class="active fa fa-star" aria-hidden="true"></i>
+          </label>
+          <input id="star-2" type="radio" name="rating" value="star-2" />
+          <label for="star-2" title="2 stars">
+              <i class="active fa fa-star" aria-hidden="true"></i>
+          </label>
+          <input id="star-1" type="radio" name="rating" value="star-1" />
+          <label for="star-1" title="1 star">
+              <i class="active fa fa-star" aria-hidden="true"></i>
+          </label>
+        </div>
       </div>
     );
   }
